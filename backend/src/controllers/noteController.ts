ea915ed2b9008fbaede4681cc94c3334780f5f7f
Teodora <vestale.teodora00@gmail.com>
2025-03-17 13:36:25 +0200

    feat: delete note & copy to clipboard feature

--- backend/src/controllers/noteController.ts
+++ backend/src/controllers/noteController.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../db.ts";
 import { Note } from "../models/Note.ts";
-import { getChatCompletion, getChatCompletion2 } from "../openrouter.ts";
+import { getChatCompletion } from "../openrouter.ts";
 
 const noteRepository = AppDataSource.getRepository(Note);
 
@@ -13,7 +13,6 @@ export const saveNote = async (req: Request, res: Response) => {
     return;
   }
   try {
-    console.log(summary);
     const newNote = noteRepository.create({ content: summary });
     await noteRepository.save(newNote);
     res.status(201).json({ message: "Note saved successfully", note: newNote });
@@ -21,6 +20,32 @@ export const saveNote = async (req: Request, res: Response) => {
   } catch (err: any) {
     console.error(err.message);
     res.status(200).json({ message: err.message });
+    return;
+  }
+};
+
+export const deleteNote = async (req: Request, res: Response) => {
+  let { id } = req.params;
+  const numericId = Number(id);
+  if (!numericId) {
+    res.status(400).json({ error: "Note ID is required." });
+    return;
+  }
+
+  try {
+    const note = await noteRepository.findOneBy({ id: numericId });
+
+    if (!note) {
+      res.status(404).json({ error: "Note not found." });
+      return;
+    }
+
+    await noteRepository.delete(id);
+    res.status(200).json({ message: "Note deleted successfully." });
+    return;
+  } catch (err: any) {
+    console.error("Error deleting note:", err.message);
+    res.status(500).json({ error: "Internal server error." });
     return;
   }
 };
@@ -55,6 +80,5 @@ export const generateSummary = async (req: Request, res: Response) => {
   const prompt = `Summarize the following text in a ${tone} tone. The summary must have maximum ${lengthLimits[length]} characters. Keep key details and ensure clarity:\n\n"${noteText}"`;
 
   console.log(`Prompt: ${prompt}`);
-  // await getChatCompletion2(prompt);
   await getChatCompletion(prompt, req, res, temperature);
 };
--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -1,7 +1,19 @@
 import { useEffect, useState } from "react";
-import { Paper, Typography, List, ListItem, ListItemText } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import {
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Tooltip,
+  Button,
+} from "@mui/material";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { NotesApi } from "../api/index";
 import { formatStreamingText } from "../utils/formatStreamingText";
+import pink from "@mui/material/colors/pink";
 
 // Define Note type
 interface Note {
@@ -14,6 +26,7 @@ export const Notes = () => {
   const [notes, setNotes] = useState<Note[]>([]);
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
   const [errMessage, setErrMessage] = useState<string>("");
+  const [copied, setCopied] = useState(false);
 
   const Api = new NotesApi();
 
@@ -22,10 +35,39 @@ export const Notes = () => {
   }, []);
 
   useEffect(() => {
+    getNotes();
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  useEffect(() => {
     if (notes.length > 0) {
-      setSelectedNote(notes[0]); // Set the latest note by default
+      setSelectedNote(notes[0]);
     }
   }, [notes]);
+
+  const handleCopy = async (noteText: string) => {
+    try {
+      await navigator.clipboard.writeText(noteText);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const handleDeleteNote = async (id: Number) => {
+    try {
+      if (!id) {
+        return;
+      }
+      const response = await Api.handleDeleteNote(id);
+      console.log(response);
+      getNotes();
+    } catch (err: any) {
+      console.error("Could not save note", err);
+      setErrMessage(err.message);
+    }
+  };
 
   const getNotes = async () => {
     try {
@@ -42,30 +84,37 @@ export const Notes = () => {
   };
 
   return (
-    <div className="container mx-auto p-6 flex gap-4">
+    <div className="container mx-auto p-6 flex gap-4 flex h-screen">
       {/* Left Panel - Notes List */}
-      <div className="w-1/3 border-r border-gray-300 h-screen overflow-auto">
-        <Typography variant="h4" className="p-4 font-bold">
-          Notes
-        </Typography>
-        <List>
-          {notes.map((note) => (
-            <ListItem
-              component="button"
-              key={note.id}
-              onClick={() => setSelectedNote(note)}
-              sx={{
-                bgcolor: selectedNote?.id === note.id ? "#f0f0f0" : "white",
-                borderBottom: "1px solid #ddd",
-              }}
-            >
-              <ListItemText
-                primary={note.content.substring(0, 30) + "..."}
-                secondary={new Date(note.createdAt).toLocaleDateString()}
-              />
-            </ListItem>
-          ))}
-        </List>
+      <div className="w-1/3 border-r border-gray-300 h-full flex flex-col">
+        <div className="p-2 font-bold sticky top-0  z-10">
+          <Typography variant="h4">Notes</Typography>
+        </div>
+        <div className="overflow-y-auto" style={{ maxHeight: "80vh" }}>
+          <List>
+            {notes.map((note) => (
+              <ListItem
+                component="button"
+                key={note.id}
+                onClick={() => setSelectedNote(note)}
+                sx={{
+                  bgcolor: selectedNote?.id === note.id ? "#f0f0f0" : "white",
+                  borderBottom: "1px solid #ddd",
+                }}
+              >
+                <ListItemText
+                  primary={note.content.substring(0, 50) + "..."}
+                  secondary={new Date(note.createdAt).toLocaleDateString()}
+                />
+                <Grid size={{ xs: 8 }} sx={{ color: pink[500] }}>
+                  <DeleteOutlinedIcon
+                    onClick={() => handleDeleteNote(note.id)}
+                  />
+                </Grid>
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
 
       {/* Right Panel - Selected Note */}
@@ -82,7 +131,18 @@ export const Notes = () => {
               boxShadow: "4px 4px 10px rgba(0,0,0,0.2)",
               transform: `rotate(${Math.random() * 6 - 3}deg)`,
             }}
+            className="!copy-box"
           >
+            <Tooltip
+              title={copied ? "Copied! Paste away. 🚀" : "Copy to clipboard"}
+            >
+              <Button
+                onClick={() => handleCopy(selectedNote.content)}
+                sx={{ minWidth: "30px" }}
+              >
+                <ContentCopyIcon sx={{ height: "20px", color: "#216C17" }} />
+              </Button>
+            </Tooltip>
             <Typography
               variant="body1"
               sx={{ fontWeight: "bold", color: "black" }}
